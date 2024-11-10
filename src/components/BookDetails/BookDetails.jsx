4bd41a1c7bc5e5e6e34fa09de6315f82d3e17518
Abrar <ahmedabrararian@gmail.com>
2024-11-10 07:00:54 +0600

    Added Sonner for Toasts

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,9 +1,9 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Tag from "../Tag/Tag";
 import { addingIdToReadList, addingIdtoWishList } from "../Utility/addToDB";
+import { Toaster } from "sonner";
 
 const BookDetails = () => {
-
   // Getting the specific id, loading data & destructuring:
   const { bookId } = useParams();
   const id = parseInt(bookId);
@@ -11,64 +11,84 @@ const BookDetails = () => {
   const data = useLoaderData();
   const bookData = data.find((book) => book.bookId === id);
 
-  const { image, bookName, author, category, review, tags, totalPages, publisher, yearOfPublishing, rating, } = bookData;
+  const {
+    image,
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = bookData;
 
   // Saving in local storage for readlist:
   const handleAddToReadList = (id) => {
     addingIdToReadList(id);
-  }
+  };
 
   const handleAddToWishList = (id) => {
     addingIdtoWishList(id);
-  }
+  };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-5 md:gap-10 my-10">
-      <div className="bg-gray-100 rounded-2xl flex justify-center items-center p-20">
-        <img className="object-cover max-h-[600px]" src={image} alt="" />
-      </div>
-      <div className="flex flex-col gap-5">
-        <p className="font-PlayfairD font-bold text-4xl">{bookName}</p>
-        <p className="font-medium text-xl opacity-80">By: {author}</p>
-        <hr />
-        <p className="font-medium opacity-80">{category}</p>
-        <hr />
-        <p>
-          <span className="font-semibold">Review: </span>
-          {review}
-        </p>
-        <div className="flex items-center gap-3">
-          <p className="font-semibold">Tags</p>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-5 md:gap-10 my-10">
+        <div className="bg-gray-100 rounded-2xl flex justify-center items-center p-20">
+          <img className="object-cover max-h-[600px]" src={image} alt="" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <p className="font-PlayfairD font-bold text-4xl">{bookName}</p>
+          <p className="font-medium text-xl opacity-80">By: {author}</p>
+          <hr />
+          <p className="font-medium opacity-80">{category}</p>
+          <hr />
+          <p>
+            <span className="font-semibold">Review: </span>
+            {review}
+          </p>
           <div className="flex items-center gap-3">
-            {tags.map((tag, index) => (
-              <Tag key={index} tag={tag}></Tag>
-            ))}
+            <p className="font-semibold">Tags</p>
+            <div className="flex items-center gap-3">
+              {tags.map((tag, index) => (
+                <Tag key={index} tag={tag}></Tag>
+              ))}
+            </div>
+          </div>
+          <hr />
+          <p>
+            Number of Pages: <span className="font-semibold">{totalPages}</span>
+          </p>
+          <p>
+            Publisher: <span className="font-semibold">{publisher}</span>
+          </p>
+          <p>
+            Year of Publishing:{" "}
+            <span className="font-semibold">{yearOfPublishing}</span>
+          </p>
+          <p>
+            Rating: <span className="font-semibold">{rating}</span>
+          </p>
+          <div className="flex gap-3">
+            <button
+              onClick={() => handleAddToReadList(id)}
+              className="btn hover:bg-primGreen border border-primGreen hover:text-white bg-transparent text-primGreen text-base shadow-none"
+            >
+              Mark As Read
+            </button>
+            <button
+              onClick={() => handleAddToWishList(id)}
+              className="btn hover:bg-[#50B1C9] hover:text-white border border-[#50B1C9] bg-transparent hover:border-[#50B1C9] text-[#50B1C9] text-base"
+            >
+              Add To Wishlist
+            </button>
           </div>
         </div>
-        <hr />
-        <p>
-          Number of Pages: <span className="font-semibold">{totalPages}</span>
-        </p>
-        <p>
-          Publisher: <span className="font-semibold">{publisher}</span>
-        </p>
-        <p>
-          Year of Publishing:{" "}
-          <span className="font-semibold">{yearOfPublishing}</span>
-        </p>
-        <p>
-          Rating: <span className="font-semibold">{rating}</span>
-        </p>
-        <div className="flex gap-3">
-          <button onClick={() => handleAddToReadList(id)} className="btn hover:bg-primGreen border border-primGreen hover:text-white bg-transparent text-primGreen text-base shadow-none">
-            Mark As Read
-          </button>
-          <button onClick={() => handleAddToWishList(id)} className="btn hover:bg-[#50B1C9] hover:text-white border border-[#50B1C9] bg-transparent hover:border-[#50B1C9] text-[#50B1C9] text-base">
-            Add To Wishlist
-          </button>
-        </div>
       </div>
-    </div>
+      <Toaster position="top-center" richColors />
+    </>
   );
 };
 
--- src/components/Utility/addToDB.js
+++ src/components/Utility/addToDB.js
@@ -1,67 +1,74 @@
+import { toast } from "sonner";
+
+
 const theReadListIdHolder = () => {
-    const storedListStr = localStorage.getItem('read-list'); // add kora string ta nilam local theke
-    if (storedListStr) {
-        const storedList = JSON.parse(storedListStr); // string ta ke abar json obj banalam
-        return storedList;
-    } else {
-        return [];
-    }
-}
+  const storedListStr = localStorage.getItem("read-list"); // add kora string ta nilam local theke
+  if (storedListStr) {
+    const storedList = JSON.parse(storedListStr); // string ta ke abar json obj banalam
+    return storedList;
+  } else {
+    return [];
+  }
+};
 
 const addingIdToReadList = (id) => {
-    const existingList = theReadListIdHolder();
-    if (existingList.includes(id)) {
-        return alert("Already read this book!");
-    }
-    else {
-        existingList.push(id);
-        // also sending this data to local-storage:
-        const storedListStr = JSON.stringify(existingList); // karon local storage e string hishebe e rakha lage
-        localStorage.setItem('read-list', storedListStr); // ki naam e rakhbo, ki rakhbo - ei duita parameter deya lage
-        // ekhane const er naam same dibo naki na (storedListStr), doesn't matter. kintu ki naam e store kortesi (ekhane 'read-list', eta important)
+  const existingList = theReadListIdHolder();
+  if (existingList.includes(id)) {
+    toast.error("Already read this book!");
+  } else {
+    existingList.push(id);
+    // also sending this data to local-storage:
+    const storedListStr = JSON.stringify(existingList); // karon local storage e string hishebe e rakha lage
+    localStorage.setItem("read-list", storedListStr); // ki naam e rakhbo, ki rakhbo - ei duita parameter deya lage
+    // ekhane const er naam same dibo naki na (storedListStr), doesn't matter. kintu ki naam e store kortesi (ekhane 'read-list', eta important)
+    toast.success("Added Book to your Read-List");
 
-        // Removing the book from wish-list after it's read (amar paknami): 
-        const checkWishListStr = localStorage.getItem('wish-list');
-        const checkWishList = JSON.parse(checkWishListStr);
-        if (checkWishList.includes(id)) {
-            const index = checkWishList.indexOf(id);
-            if (index > -1) {
-                checkWishList.splice(index, 1);
-                const storedListStr = JSON.stringify(checkWishList);
-                localStorage.setItem('wish-list', storedListStr);
-            }
-        } // kaaj korse yo!
-    }
-}
+    // Removing the book from wish-list after it's read (amar paknami):
+    const checkWishListStr = localStorage.getItem("wish-list");
+    const checkWishList = JSON.parse(checkWishListStr);
+    if (checkWishList.includes(id)) {
+      const index = checkWishList.indexOf(id);
+      if (index > -1) {
+        checkWishList.splice(index, 1);
+        const storedListStr = JSON.stringify(checkWishList);
+        localStorage.setItem("wish-list", storedListStr);
+      }
+    } // kaaj korse yo!
+  }
+};
 
 const theWishListIdHolder = () => {
-    const storedListStr = localStorage.getItem('wish-list');
-    if (storedListStr) {
-        const storedList = JSON.parse(storedListStr);
-        return storedList;
-    } else {
-        return [];
-    }
-}
+  const storedListStr = localStorage.getItem("wish-list");
+  if (storedListStr) {
+    const storedList = JSON.parse(storedListStr);
+    return storedList;
+  } else {
+    return [];
+  }
+};
 
 const addingIdtoWishList = (id) => {
-    const existingList = theWishListIdHolder();
-    const checkReadListStr = localStorage.getItem('read-list');
-    const checkReadList = JSON.parse(checkReadListStr);
-    if (checkReadList.includes(id)) {
-        alert("You've already read this book!")
+  const existingList = theWishListIdHolder();
+  const checkReadListStr = localStorage.getItem("read-list");
+  const checkReadList = JSON.parse(checkReadListStr);
+  if (checkReadList.includes(id)) {
+    toast.error("Read Books can't be in Wish-List!");
+  } else {
+    if (existingList.includes(id)) {
+        toast.error("Book already exists in Wish-List!");
     } else {
-        if (existingList.includes(id)) {
-            alert("This book already exists in your Wishlist!");
-        } else {
-            existingList.push(id);
-            const storedListStr = JSON.stringify(existingList);
-            localStorage.setItem('wish-list', storedListStr);
-        }
+      existingList.push(id);
+      const storedListStr = JSON.stringify(existingList);
+      localStorage.setItem("wish-list", storedListStr);
+      toast.success("Added Book to your Wish-List!");
     }
-    
-}
+  }
+};
 
-
-export { addingIdToReadList, addingIdtoWishList, theReadListIdHolder, theWishListIdHolder }
+export {
+  addingIdToReadList,
+  addingIdtoWishList,
+  theReadListIdHolder,
+  theWishListIdHolder,
+};
 // ei export e default dile import er time e show e kore na.
